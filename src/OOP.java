86e1a0b10d3bf409f46f2fcc4a2da5f4e92958f1
Abhishek-Atole <abhiatole17@gmail.com>
2025-06-03 23:12:47 +0530

    Arithmatic Operation (Addition ,Substraction)

--- src/OOP.java
+++ src/OOP.java
@@ -1,47 +1,88 @@
+// Importing the Scanner class from java.util package for user input
 import java.util.*;
 
-class Arithematic {                                 // Constructor
-    public int Value1;
-    public int Value2;
+// Class definition for Arithmetic operations
+class Arithmetic {
 
-    // Arithematic aobj = new Arithematic (No1,No2)
-    // Arithematic aobj = new Arithematic (15,7);
+    // Instance variables to hold the two numbers
+    public int value1;
+    public int value2;
 
-    public Arithematic(int a, int b) {
-        this.Value1 = a;
-        this.Value2 = b;
-
+    // Parameterized constructor to initialize value1 and value2
+    public Arithmetic(int a, int b) {
+        this.value1 = a;  // Assigning parameter 'a' to instance variable 'value1'
+        this.value2 = b;  // Assigning parameter 'b' to instance variable 'value2'
     }
 
-    public int Addition() {
-        int Result = 0;
-        Result = this.Value1 + this.Value2;
-        return Result;
+    // Method to perform addition
+    public int addition() {
+        int result = this.value1 + this.value2;  // Calculate sum
+        return result;  // Return result
     }
 
-    public int Substraction() {
-        int Result = 0;
-        Result = this.Value1 - this.Value2;
-        return Result;
+    // Method to perform subtraction
+    public int subtraction() {
+        int result = this.value1 - this.value2;  // Calculate difference
+        return result;  // Return result
+    }
+
+    // (Optional) Method to perform multiplication
+    public int multiplication() {
+        int result = this.value1 * this.value2;
+        return result;
+    }
+
+    // (Optional) Method to perform division
+    public int division() {
+        if (this.value2 != 0) {
+            return this.value1 / this.value2;
+        } else {
+            System.out.println("Error: Division by zero is not allowed.");
+            return 0;
+        }
     }
 }
 
+// Main class
 public class OOP {
-    public static void main(String Args[]) {
-        int No1 = 0, No2 = 0, Ans = 0;
-        Scanner sobj = new Scanner(System.in);
 
-        System.out.println("Enter the First Number : ");
-        No1 = sobj.nextInt();
-        System.out.println("Enter the Second Number : ");
-        No2 = sobj.nextInt();
+    // Entry point of the program
+    public static void main(String[] args) {
 
-        Arithematic aobj = new Arithematic(No1, No2);
+        // Variable declarations
+        int no1 = 0, no2 = 0, ans = 0;
 
-        Ans = aobj.Addition();
-        System.out.println("Addition is : " + Ans);
-        Ans = aobj.Substraction();
-        System.out.println("Substraction is : " + Ans);
-        sobj.close();
+        // Scanner object to take input from the user
+        Scanner scanner = new Scanner(System.in);
+
+        // Asking the user to enter the first number
+        System.out.print("Enter the first number: ");
+        no1 = scanner.nextInt();  // Read integer input
+
+        // Asking the user to enter the second number
+        System.out.print("Enter the second number: ");
+        no2 = scanner.nextInt();  // Read integer input
+
+        // Creating an object of Arithmetic class using the constructor
+        Arithmetic arithmeticObject = new Arithmetic(no1, no2);
+
+        // Performing addition and printing the result
+        ans = arithmeticObject.addition();
+        System.out.println("Addition is: " + ans);
+
+        // Performing subtraction and printing the result
+        ans = arithmeticObject.subtraction();
+        System.out.println("Subtraction is: " + ans);
+
+        // (Optional) Performing multiplication and printing the result
+        ans = arithmeticObject.multiplication();
+        System.out.println("Multiplication is: " + ans);
+
+        // (Optional) Performing division and printing the result
+        ans = arithmeticObject.division();
+        System.out.println("Division is: " + ans);
+
+        // Closing the scanner object to release resources
+        scanner.close();
     }
 }
