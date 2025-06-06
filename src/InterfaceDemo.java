b12191f8273e8db169a4a20b3f011368a48656ae
Abhishek-Atole <abhiatole17@gmail.com>
2025-06-07 01:41:00 +0530

    Interface Concept in Java

---
+++ src/InterfaceDemo.java
@@ -0,0 +1,26 @@
+interface Mathematics {
+    public int Addition(int A, int B);
+
+    public int Substraction(int A, int B);
+}
+
+class Marvellous implements Mathematics {
+    public int Addition(int A, int B) {
+        return A + B;
+    }
+
+    public int Substraction(int A, int B) {
+        return A - B;
+    }
+}
+
+class InterfaceDemo {
+    public static void main(String Args[]) {
+        Marvellous mobj = new Marvellous();
+        int iRet = 0;
+        iRet = mobj.Addition(11, 21);
+        System.out.println("Addition is : "+iRet);
+        iRet = mobj.Substraction(32, 52);
+        System.out.println("Substraction is : "+iRet);
+    }
+}
---
+++ src/InterfaceDemo1.java
@@ -0,0 +1,30 @@
+interface Circle {
+    float PI = 3.14f;
+
+    float Area(float Radius);
+
+    float Circumference(float Radius);
+}
+
+class PPA implements Circle {
+    public float Area(float Radius) {
+        return PI * Radius * Radius;
+    }
+
+    public float Circumference(float Radius) {
+        return 2 * PI * Radius;
+    }
+}
+
+public class InterfaceDemo1 {
+    public static void main(String Args[]) {
+        PPA pobj = new PPA();
+        System.out.println("Value of PI : " +Circle.PI);
+        float fRet = 0.0f;
+        fRet = pobj.Area(10.7f);
+        System.out.println("Area is : " + fRet);
+        fRet = pobj.Circumference(10.7f);
+        System.out.println("Circumference is : " + fRet);
+    }
+
+}
