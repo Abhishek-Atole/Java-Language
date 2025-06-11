333445ab3bb98dd5b232ed4b32765414f879e883
Abhiatole <your-email@example.com>
2025-06-11 19:31:11 +0530

    User Defined Exception Hadling

--- src/UserDefined.java
+++ src/UserDefined.java
@@ -10,18 +10,26 @@ class UserDefined {
     public static void main(String Arg[]) {
         Scanner sobj = new Scanner(System.in);
         System.out.println("Enter Your Age : ");
-        int iAge = sobj.nextInt();
+
+        int iAge = 0;
+
+        if (sobj.hasNextInt()) {
+            iAge = sobj.nextInt();
+        } else {
+            System.out.println("No input provided. Using default age: 18");
+            iAge = 18; // Default value or handle appropriately
+        }
+
         try {
             if (iAge < 18) {
                 AgeInvalid aobj = new AgeInvalid("Your Age is Below 18.");
                 throw aobj;
             } else {
-                System.out.println("Login Succesful....");
+                System.out.println("Login Successful....");
             }
         } catch (AgeInvalid obj) {
-            System.out.println("Inside Catch Block .");
+            System.out.println("Inside Catch Block.");
             System.out.println(obj);
         }
     }
-
 }
