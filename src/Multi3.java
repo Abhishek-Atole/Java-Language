63b22662a25ceed03023e843bf9901073d3874bb
Abhiatole <your-email@example.com>
2025-06-22 20:49:42 +0530

    MultiThreading in Java

--- src/Multi3.java
+++ src/Multi3.java
@@ -1,5 +1,5 @@
 class Demo extends Thread {
-    public void run() {
+    public void run() {         //Running State
         String name = Thread.currentThread().getName();
         System.out.println("Current Thread is : " + name);
         for (int i = 1; i < 1000; i++) {
@@ -14,14 +14,14 @@ class Multi3 {
     public static void main(String Arg[]) {
         System.out.println("Current Thread is : " + Thread.currentThread().getName());
 
-        Demo obj1 = new Demo();
-        Demo obj2 = new Demo();
+        Demo obj1 = new Demo(); //New State 
+        Demo obj2 = new Demo(); //New State
 
-        obj1.setName("First_Thread");
+        obj1.setName("First_Thread");   
         obj2.setName("Second_Threaad");
 
-        obj1.start();
-        obj2.start();
+        obj1.start();   //Runnable State 
+        obj2.start();   //Runnable State
 
     }
 
---
+++ src/Multi5.java
@@ -0,0 +1,24 @@
+class Demo extends Thread {
+    public void run() {
+        for (int i = 1; i < 10; i++) {
+            try {
+                System.out.println("Value of i is : " + i);
+                Thread.sleep(5000);
+            } catch (Exception obj) {
+            }
+        }
+    }
+}
+
+class Multi5 {
+    public static void main(String Arg[]) throws Exception {
+        System.out.println("Current Thread is : " + Thread.currentThread().getName());
+
+        Demo obj1 = new Demo();
+        obj1.start();
+        obj1.join();
+        System.out.println("End of Main Thread");
+
+    }
+
+}
