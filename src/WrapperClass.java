9c89209368d8e41327a0133164e2f7bf711c40a8
Abhiatole <your-email@example.com>
2025-06-29 17:46:19 +0530

    Wrapper CLass

---
+++ src/WrapperClass.java
@@ -0,0 +1,14 @@
+class WrapperClass {
+    public static void main(String Arg[]) {
+        int no = 11;
+        Integer iobj = no;
+
+        System.out.println(no);
+        System.out.println(iobj);
+
+        int i = iobj;
+
+        System.out.println(i);
+    }
+
+}
